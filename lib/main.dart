38d09498c34345f158605b16fa0ccfffc256d943
Ritik <dritik458@gmail.com>
2024-02-28 11:49:03 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          minimum: EdgeInsets.all(8),
+          child: Text(
+            'Hello',
+            style: TextStyle(color: Colors.blue, fontSize: 50),
+          ),
+        ),
+      ),
+    );
+  }
+}
